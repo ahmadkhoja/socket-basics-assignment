c5e3acde4e97583fb8d863a3d124d2d572f1e9ec
ahmadkhoja <ahmadkhoja96@gmail.com>
2018-05-07 13:29:43 +0300

    part 1 done

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+class App extends React.Component{
+  
+    state = { 
+      socket:null, 
+      globalNumber:0 
+    }
+  
+    componentDidMount(){
+      const socket = io('http://localhost:8888');
+  
+      this.setState({socket})
+  
+      socket.on('number:change', (globalNumber) => {
+        this.setState({globalNumber})
+      })
+  
+    }
+  
+    onIncrement = () => this.state.socket.emit('increment')
+    onDecrement = () => this.state.socket.emit('decrement')
+    render(){
+      return(
+        <div>
+          hello
+        </div>
+        // do something here to show the globalNumber and use increment and decrement
+      )
+    }
   }
-}
 
 export default App;
